4de6c2f0193bc1f6ca9ab259d594cce4069cf3b6
Hasitha Wickramage <hasithawickramage@gmail.com>
2025-10-28 17:19:43 +1300

    config pytest to requirement.txt

--- api/tests.py
+++ api/tests.py
@@ -49,19 +49,10 @@ class ActivityTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(len(response.data) > 0)
 
-    def test_delete_activity(self):
-        activity = Activity.objects.create(user="self.user.username", activity_type="Workout", status="planned")
-        url = reverse('activity-detail', args=[activity.id])
-        response = self.client.delete(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Activity.objects.count(), 0)
-
-# def test_update_activity(self):
-#     activity = Activity.objects.create(user="self.user.username", activity_type="Workout", status="planned")
-#     url = reverse('activity-detail', args=[activity.id])
-#     data = {"activity_type": "Meal", "status": "completed"}
-#     response = self.client.put(url, data, format='json')
-#     self.assertEqual(response.status_code, status.HTTP_200_OK)
-#     self.assertEqual(Activity.objects.get().activity_type, "Meal")
-
+def test_delete_activity(self):
+    activity = Activity.objects.create(user=self.user.username, activity_type="Workout", status="planned")
+    url = reverse('activity-detail', args=[activity.id])
+    response = self.client.delete(url, format='json')
+    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    self.assertEqual(Activity.objects.count(), 0)
 
